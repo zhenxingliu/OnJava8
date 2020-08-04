d90e19645ee2ae6955e04a71e5571fb2aab53423
liuzhenxing <liuzhenxing@mochasoft.com.cn>
2020-08-04 15:09:34 +0800

    init

---
+++ stream/src/main/java/com/blueheart/onjava/App.java
@@ -0,0 +1,13 @@
+package com.blueheart.onjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
